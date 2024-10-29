e0f289f4e4bb3a857a59a48698ef907f54a27e73
Timi <alabitimi65@gmail.com>
2024-10-28 22:19:46 -0600

    add file counter for google drive

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const { rsort } = require('semver')
 const { startWatcher, stopWatcher, getStatus } = require('../watchers/fileWatcher')
 const router = express.Router()
+const config = require('../config')
+const { countFilesByExtension } = require('../utils/fileCounter')
+const logger = require('../utils/logger')
 
 router.post('/start', (req, res) => {
         startWatcher()
@@ -10,11 +13,23 @@ router.post('/start', (req, res) => {
 
 router.post('/stop', (req, res) => {
         stopWatcher()
-        rsort.send('File watcher stopped')
+        res.send('File watcher stopped')
 })
 
 router.get('/status', (req, res) => {
         res.send(getStatus())
+})
+
+// File counter route
+router.get('/files', (req, res) => {
+        try {
+                const fileData = countFilesByExtension(config.watchFolder)
+                res.json(fileData)
+        } catch (error) {
+                console.log(error)
+                logger.error('Error counting files: ', error)
+                res.status(500).json({ message: "Error counting files" })
+        }
 })
 
 module.exports = router
